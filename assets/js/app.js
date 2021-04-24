1909d5efa733c9302d0c6706713a7ef929e2b530
gucaselo <gucaselo@gmail.com>
2021-04-24 02:08:11 -0400

    Chart updating on both axis except back to default

--- assets/js/app.js
+++ assets/js/app.js
@@ -128,35 +128,98 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
     var leftMargin = margin.left;
     var bottomMargin = height + margin.top;
     var counter = 0;
-    for(i=0; i<xAxisTexts.length; i++) {
-        
-        leftMargin -= 20;
-        bottomMargin += 12;
+    
+    // X Axis Labels
+    var xPoverty = chartGroup.append("g")
+    .append("text")
+    .attr("transform", `translate(${width / 2}, ${bottomMargin + 12})`)
+    .attr("class", "aText")
+    .text("In Poverty(%)")
+    .classed("labels", true)
+    .classed("active", true);
 
-        var yLabels = chartGroup.append("g")
+    var xAge = chartGroup.append("g")
+    .append("text")
+    .attr("transform", `translate(${width / 2}, ${bottomMargin + 24})`)
+    .attr("class", "aText")
+    .text("Age (Median)")
+    .classed("labels", true)
+    .classed("inactive", true);
+
+    var xIncome = chartGroup.append("g")
+    .append("text")
+    .attr("transform", `translate(${width / 2}, ${bottomMargin + 36})`)
+    .attr("class", "aText")
+    .text("Household Income (Median)")
+    .classed("labels", true)
+    .classed("inactive", true);
+
+    // Y Axis Labels
+    var yHealthcare = chartGroup.append("g")
         .append("text")   
-        // .data(yAxisTexts)
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - leftMargin)
+        .attr("y", 0 - leftMargin + 60)
         .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .attr("class", "aText")
-        // Reverse the order for appending y labels because of the order of margins increments
-        .text(yAxisTexts[(yAxisTexts.length-1) - i])
+        .text("Lacks Healthcare (%)")
+        .classed("labels", true)
+        .classed("active", true);
+    
+    var ySmokes = chartGroup.append("g")
+        .append("text")   
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - leftMargin + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "aText")
+        .text("Smokes (%)")
         .classed("labels", true)
         .classed("inactive", true);
 
-        var xLabels = chartGroup.append("g")
-        .append("text")
-        // .data(x)
-        .attr("transform", `translate(${width / 2}, ${bottomMargin})`)
+    var yObese = chartGroup.append("g")
+        .append("text")   
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - leftMargin + 20)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
         .attr("class", "aText")
-        // .text("In Poverty(%)");
-        .text(xAxisTexts[i])
+        .text("Obese (%)")
         .classed("labels", true)
         .classed("inactive", true);
+
+
+
+    ///////////////////////////////////
+    // for(i=0; i<xAxisTexts.length; i++) {
         
-    };
+    //     leftMargin -= 20;
+    //     bottomMargin += 12;
+
+    //     var yLabels = chartGroup.append("g")
+    //     .append("text")   
+    //     // .data(yAxisTexts)
+    //     .attr("transform", "rotate(-90)")
+    //     .attr("y", 0 - leftMargin)
+    //     .attr("x", 0 - (height / 2))
+    //     .attr("dy", "1em")
+    //     .attr("class", "aText")
+    //     // Reverse the order for appending y labels because of the order of margins increments
+    //     .text(yAxisTexts[(yAxisTexts.length-1) - i])
+    //     .classed("labels", true)
+    //     .classed("inactive", true);
+
+        // var xLabels = chartGroup.append("g")
+        // .append("text")
+        // // .data(x)
+        // .attr("transform", `translate(${width / 2}, ${bottomMargin})`)
+        // .attr("class", "aText")
+        // // .text("In Poverty(%)");
+        // .text(xAxisTexts[i])
+        // .classed("labels", true)
+        // .classed("inactive", true);
+        
+    // };
 
     // Create Circles
     // ==============================
@@ -257,32 +320,94 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
      d3.selectAll(".labels").on("click", function(d) {
         var selection = d3.select(this).text();
         // axisLabels(xAxisTexts, yAxisTexts);
-        // var xValue;
+        console.log(selection)
+
         if (selection === "In Poverty (%)" || selection === "Age (Median)" || selection === "Household Income (Median)") {
             if (selection === "In Poverty (%)") {
                 xValue = "poverty";
+                xPoverty
+                    .classed("active", true)
+                    .classed("inactive", false);
+                xAge
+                    .classed("active", false)
+                    .classed("inactive", true);
+                xIncome
+                    .classed("active", false)
+                    .classed("inactive", true);
+
             }
             else if (selection === "Age (Median)") {
                 xValue = "age";
+                xPoverty
+                    .classed("active", false)
+                    .classed("inactive", true);
+                xAge
+                    .classed("active", true)
+                    .classed("inactive", false);
+                xIncome
+                    .classed("active", false)
+                    .classed("inactive", true);
             }
             else if (selection === "Household Income (Median)"){
                 xValue = "income";
+                xPoverty
+                    .classed("active", false)
+                    .classed("inactive", true);
+                xAge
+                    .classed("active", false)
+                    .classed("inactive", true);
+                xIncome
+                    .classed("active", true)
+                    .classed("inactive", false);
             }
         }
 
         else if (selection === "Obese (%)" || selection === "Smokes (%)" || selection === "Lacks Healthcare(%)") {
             if (selection === "Obese (%)") {
                 yValue = "obesity";
+                
+                yHealthcare
+                    .classed("active", false)
+                    .classed("inactive", true);
+                ySmokes
+                    .classed("active", false)
+                    .classed("inactive", true);
+                yObese
+                    .classed("active", true)
+                    .classed("inactive", false);
+
             }
             else if (selection === "Smokes (%)") {
                 yValue = "smokes";
+
+                yHealthcare
+                    .classed("active", false)
+                    .classed("inactive", true);
+                ySmokes
+                    .classed("active", true)
+                    .classed("inactive", false);
+                yObese
+                    .classed("active", false)
+                    .classed("inactive", true);
+
             }
             else if (selection === "Lacks Healthcare(%)"){
                 yValue = "healthcare";
+
+                yHealthcare
+                    .classed("active", true)
+                    .classed("inactive", false);
+                ySmokes
+                    .classed("active", false)
+                    .classed("inactive", true);
+                yObese
+                    .classed("active", false)
+                    .classed("inactive", true);
+
             }
         }
-        d3.select(this).classed("active", true);
-        d3.select(this).classed("inactive", false);
+        // d3.select(this).classed("active", true);
+        // d3.select(this).classed("inactive", false);
          
         // var selection = d3.select(this).text();
         updatePlot(selection, xValue, yValue);
